a2e34ea90fb1e4ab3ee4630e6b597c3575233901
Evan Light <evan@tripledogdare.net>
2014-03-11 17:20:48 +0000

    WIP removing most instance variables and auth by token

--- lib/fog/openstackcommon/identity.rb
+++ lib/fog/openstackcommon/identity.rb
@@ -1,5 +1,4 @@
 require_relative './core'
-require_relative './identity/authenticator'
 
 module Fog
   module Identity
@@ -143,11 +142,10 @@ module Fog
       end
 
       class Real
-        attr_reader :current_user, :current_tenant, :unscoped_token
+        attr_reader :current_user, :current_tenant
         attr_reader :auth_token
 
         def initialize(options={})
-          # puts "===== Fog::Identity::OpenStackCommon -> initialize ====="
           apply_options(options)
           authenticate
           connection_url = "#{@scheme}://#{@host}:#{@port}"
@@ -155,12 +153,10 @@ module Fog
         end
 
         def reload
-          # puts "===== Fog::Identity::OpenStackCommon -> reload ====="
           @connection.reset
         end
 
         def request(params)
-          # puts "===== Fog::Identity::OpenStackCommon -> request ====="
           retried = false
           begin
             response = @connection.request(params.merge({
@@ -207,9 +203,8 @@ module Fog
 
         def authenticate
           # puts "===== Fog::Identity::OpenStackCommon -> authenticate ====="
-          if !@openstack_management_url || @openstack_must_reauthenticate
-            options = init_auth_options
-            credentials = adapter.authenticate(options, @connection_options)
+          if !@options[:openstack_management_url] || @openstack_must_reauthenticate
+            credentials = adapter.authenticate(auth_options, @connection_options)
             handle_auth_results(credentials)
           else
             @auth_token = @openstack_auth_token
@@ -220,56 +215,17 @@ module Fog
         end
 
         def apply_options(options)
-          @authenticator = options[:authenticator]
+          @options = options.dup
 
-          @openstack_auth_token = options[:openstack_auth_token]
-          # puts "openstack_auth_token: #{@openstack_auth_token}"
-
-          unless @openstack_auth_token
-            set_credentials(options)
-            validate_credentials
-          end
-
-          @openstack_tenant   = options[:openstack_tenant]
-          # puts "@openstack_tenant: #{@openstack_tenant}"
-
-          @openstack_auth_url = options[:openstack_auth_url]
-          @openstack_auth_uri = URI.parse(@openstack_auth_url)
-          # puts "@openstack_auth_url: #{@openstack_auth_url}"
-          # puts "@openstack_auth_uri: #{@openstack_auth_uri.to_yaml}"
-
-          @openstack_management_url       = options[:openstack_management_url]
-          # puts "@openstack_management_url: #{@openstack_management_url}"
-
-          @openstack_must_reauthenticate  = false
-          # puts "@openstack_must_reauthenticate: #{@openstack_must_reauthenticate}"
-
-          @openstack_service_type = options[:openstack_service_type] || ['identity']
-          # puts "@openstack_service_type: #{@openstack_service_type}"
-
-          @openstack_service_name = options[:openstack_service_name]
-          # puts "@openstack_service_name: #{@openstack_service_name}"
-
-          @openstack_current_user_id = options[:openstack_current_user_id]
-          # puts "@openstack_current_user_id: #{@openstack_current_user_id}"
-
-          @openstack_endpoint_type = options[:openstack_endpoint_type] || 'adminURL'
-          # puts "@openstack_endpoint_type: #{@openstack_endpoint_type}"
-
-          @current_user = options[:current_user]
-          # puts "@current_user: #{@current_user}"
-
-          @current_tenant = options[:current_tenant]
-          # puts "@current_tenant: #{@current_tenant}"
-
-          @connection_options = options[:connection_options] || {}
-          # puts "@connection_options: #{@connection_options}"
-
-          @persistent = options[:persistent] || false
-          # puts "@persistent: #{@persistent}"
+          @openstack_auth_uri                  = URI.parse(options[:openstack_auth_url])
+          @openstack_must_reauthenticate       = false
+          @options[:openstack_service_type]  ||= ['identity']
+          @options[:openstack_endpoint_type] ||= 'adminURL'
+          @options[:connection_options]      ||= {}
+          @options[:persistent]              ||= false
         end
 
-        def init_auth_options
+        def auth_options
           { :openstack_api_key  => @openstack_api_key,
             :openstack_username => @openstack_username,
             :openstack_auth_token => @openstack_auth_token,
@@ -288,7 +244,6 @@ module Fog
           @auth_token = credentials[:token]
           @openstack_management_url = credentials[:server_management_url]
           @openstack_current_user_id = credentials[:current_user_id]
-          @unscoped_token = credentials[:unscoped_token]
         end
 
         def save_host_attributes
@@ -303,23 +258,7 @@ module Fog
           # puts "port: #{@port}"
           # puts "scheme: #{@scheme}"
         end
-
-        def set_credentials(options={})
-          @openstack_api_key  = options[:openstack_api_key]
-          @openstack_username = options[:openstack_username]
-        end
-
-        def validate_credentials
-          missing_credentials = Array.new
-          missing_credentials << :openstack_api_key  unless @openstack_api_key
-          missing_credentials << :openstack_username unless @openstack_username
-          if !missing_credentials.empty?
-            raise ArgumentError, "Missing required arguments: #{missing_credentials.join(', ')}"
-          end
-        end
-
       end
-
     end
   end
 end
